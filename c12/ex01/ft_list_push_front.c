ad3e286ece45a755c27de5abd975cfc748b00571
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2021-07-18 19:59:50 +0200

    Finished ex1, not tested

--- c12/ex01/ft_list_push_front.c
+++ c12/ex01/ft_list_push_front.c
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list.h                                          :+:      :+:    :+:   */
+/*   ft_list_push_front.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marnaudy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/18 19:44:35 by marnaudy          #+#    #+#             */
-/*   Updated: 2021/07/18 19:53:08 by marnaudy         ###   ########.fr       */
+/*   Created: 2021/07/18 19:54:28 by marnaudy          #+#    #+#             */
+/*   Updated: 2021/07/18 19:59:27 by marnaudy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_LIST_H
-# define FT_LIST_H
-typedef struct s_list
+#include "ft_list.h"
+
+void	ft_list_push_front(t_list **begin_list, void *data)
 {
-	struct s_list	*next;
-	void			*data;
-}	t_list;
-t_list	*ft_create_elem(void *data);
-#endif
+	t_list	*save;
+
+	save = *begin_list;
+	*begin_list = ft_create_elem(data);
+	(*begin_list)->next = save;
+}

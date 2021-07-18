80b0f83c3d511c47f082ef44a13ac5997793ef3e
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2021-07-18 20:08:06 +0200

    Finished ex03, not tested

---
+++ c12/ex03/ft_list_last.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_last.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marnaudy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/18 20:04:44 by marnaudy          #+#    #+#             */
+/*   Updated: 2021/07/18 20:07:37 by marnaudy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_last(t_list *begin_list)
+{
+	if (!begin_list)
+		return (begin_list);
+	while (begin_list->next)
+		begin_list = begin_list->next;
+	return (begin_list);
+}

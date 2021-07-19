2b9396e2044f38da8bd07a6d1e652429b13ee265
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2021-07-19 21:43:58 +0200

    Finished ex6

---
+++ c13/ex06/btree_level_count.c
@@ -0,0 +1,15 @@
+#include "ft_btree.h"
+
+int	btree_level_count(t_btree *root)
+{
+	int	size_left;
+	int	size_right;
+
+	if (!root)
+		return (0);
+	size_left = btree_level_count(root->left);
+	size_right = btree_level_count(root->right);
+	if (size_left >= size_right)
+		return (size_left);
+	return (size_right);
+}

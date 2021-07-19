366a5985a1f9fd4767756bb483fc47edeb4f1338
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2021-07-19 21:24:55 +0200

    Finished ex4

---
+++ c13/ex04/btree_insert_data.c
@@ -0,0 +1,18 @@
+#include "ft_btree.h"
+
+void	btree_insert_data(t_btree **root, void *item,
+			int (*cmpf)(void *, void *))
+{
+	t_btree	*new;
+
+	if (!*root)
+	{
+		new = btree_create_node(item);
+		*root = new;
+		return ;
+	}
+	if ((*cmpf)(item, (*root)->item) < 0)
+		btree_insert_data(&(*root)->left, item, cmpf);
+	else
+		btree_insert_data(&(*root)->right, item, cmpf);
+}

eda5bbb682d9031fc57f98203a95ad3b4ab63bc1
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2021-07-19 21:37:22 +0200

    Finished ex05

---
+++ c13/ex05/btree_search_item.c
@@ -0,0 +1,19 @@
+#include "ft_btree.h"
+
+void	*btree_search_item(t_btree *root, void *data_ref,
+			int (*cmpf)(void *, void *))
+{
+	t_btree	*temp;
+
+	if (!root)
+		return (0);
+	temp = btree_search_item(root->left, data_ref, cmpf);
+	if (temp)
+		return (temp);
+	if ((*cmpf)(data_ref, root->item) == 0)
+		return (root);
+	temp = btree_search_item(root->right, data_ref, cmpf);
+	if (temp)
+		return (temp);
+	return (0);
+}

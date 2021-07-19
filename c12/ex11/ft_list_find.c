2733ab09e466312936f214580c0524fb900cdb83
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2021-07-19 10:30:52 +0200

    Finished ex11, needs testing

---
+++ c12/ex11/ft_list_find.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_find.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marnaudy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 10:26:04 by marnaudy          #+#    #+#             */
+/*   Updated: 2021/07/19 10:30:18 by marnaudy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
+{
+	while (begin_list && (*cmp)(begin_list->data, data_ref))
+		begin_list = begin_list->next;
+	return (begin_list);
+}
